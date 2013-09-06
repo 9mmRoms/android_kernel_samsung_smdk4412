8cfb0fb373d91a094db9d2c40711d601e1c9278c
andip71 <andreasp@gmx.de>
2013-09-06 17:32:47 +0200

    dhd_linux: reduce wifi wakelock times
    - reduced by factor 4
    
    Conflicts:
    	drivers/net/wireless/bcmdhd/dhd_linux.c

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -6899,10 +6899,17 @@ int dhd_os_wake_lock_timeout(dhd_pub_t *pub)
 #ifdef CONFIG_HAS_WAKELOCK
 		if (dhd->wakelock_rx_timeout_enable)
 			wake_lock_timeout(&dhd->wl_rxwake,
+<<<<<<< HEAD
 				msecs_to_jiffies(dhd->wakelock_rx_timeout_enable) >> 1);
 		if (dhd->wakelock_ctrl_timeout_enable)
 			wake_lock_timeout(&dhd->wl_ctrlwake,
 				msecs_to_jiffies(dhd->wakelock_ctrl_timeout_enable) >> 1);
+=======
+				msecs_to_jiffies(dhd->wakelock_rx_timeout_enable)/4);
+		if (dhd->wakelock_ctrl_timeout_enable)
+			wake_lock_timeout(&dhd->wl_ctrlwake,
+				msecs_to_jiffies(dhd->wakelock_ctrl_timeout_enable)/4);
+>>>>>>> a6bdd5b... dhd_linux: reduce wifi wakelock times
 #endif
 		dhd->wakelock_rx_timeout_enable = 0;
 		dhd->wakelock_ctrl_timeout_enable = 0;
